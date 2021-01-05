20f970ae8d41d8132c10d6fd95c95043edbd306c
vathana <vathana.chiv@gmail.com>
2021-01-05 15:33:02 +0100

    managementProposal et estimate

---
+++ app/Http/Controllers/estimateController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\estimate;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class estimateController extends Controller{
+    
+
+    public function createEstimate(Request $request){
+        $estimate = estimate::create($request->all());
+
+        return response()->json($estimate, 201);
+    }
+
+    public function updateEstimate($id, Request $request){
+        try{
+            $estimate = estimate::findOrFail($id);
+            $estimate->update($request->all());
+            return response()->json($estimate,200);
+        }catch(ModelNotFoundException $e){
+            return response()->json('estimation non trouvée',404);
+        }
+    }
+
+    public function deleteEstimate($id){
+        try{
+            $estimate = estimate::findOrFail($id);
+            $estimate->update(['id_tfv042119_user'=> 2]);
+            return response()->json('estimation a été archivée',200);
+        }catch(ModelNotFoundException $e){
+            return response()->json('estimation non trouvée',404);
+        }
+    }
+    
+    public function validateEstimate($id){
+        try{
+            $estimate = estimate::findOrFail($id);
+            $estimate->update(['id_tfv042119_estimate'=> 1]);
+            return response()->json('estimation a été publiée',200);
+        }catch(ModelNotFoundException $e){
+            return response()->json('estimation non trouvée',404);
+        }
+    }
+
+    public function showEstimate($id){
+        try{
+            $estimate = estimate::findOrFail($id);
+            return response()->json($estimate,200);
+        }catch(ModelNotFoundException $e){
+            return response()->json('estimation non trouvée',404);
+        }
+    }
+
+    public function showEstimateListPublished(){
+        $estimateList = estimate::where('id_tfv042119_user', 1)->get();
+        return response()->json($estimateList, 200);
+    }
+    public function showEstimateListArchive(){
+        $estimateList = estimate::where('id_tfv042119_user', 2)->get();
+        return response()->json($estimateList, 200);
+}
+}
