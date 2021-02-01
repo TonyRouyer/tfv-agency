70224c67e2362459344b1423533982e85e4a659d
Tony Rouyer <tony.rouyer@hotmail.fr>
2021-02-01 10:14:56 +0100

    maj

---
+++ app/Http/Controllers/callController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\rentalFiles;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class rentalFilesController extends Controller{
+    
+    public function addCall() {
+
+    }
+    
+    public function archiveCall(){
+
+    }
+
+}
